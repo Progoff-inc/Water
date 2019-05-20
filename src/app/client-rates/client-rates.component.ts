4014a2e625946e29dade1eca2d3cc278d3c0fcb0
Ivan Nomokonov <cpwxt38@dev.vtb>
2019-05-20 17:32:56 +0300

    тарифы

--- src/app/client-rates/client-rates.component.ts
+++ src/app/client-rates/client-rates.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Doc } from '../services/models';
 
 @Component({
   selector: 'client-rates',
@@ -6,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-rates.component.less']
 })
 export class ClientRatesComponent implements OnInit {
-
+  docs:Doc[] = [
+    {
+      Id:1,
+      Name:"ПРИКАЗ №157 ОБ УТВЕРЖДЕНИИ СБРОСА ПО СОСТАВУ СТОЧНЫХ ВОД",
+      Image:"../../assets/images/doc.png",
+      Description:"об установлении тарифов на подключение (тех. присоединение) к системе водоснабзения на 2019 год."
+    },
+    {
+      Id:1,
+      Name:"ПРИКАЗ №157 ОБ УТВЕРЖДЕНИИ СБРОСА ПО СОСТАВУ СТОЧНЫХ ВОД",
+      Image:"../../assets/images/doc.png",
+      Description:"об установлении тарифов на подключение (тех. присоединение) к системе водоснабзения на 2019 год."
+    },
+    {
+      Id:1,
+      Name:"ПРИКАЗ №157 ОБ УТВЕРЖДЕНИИ СБРОСА ПО СОСТАВУ СТОЧНЫХ ВОД",
+      Image:"../../assets/images/doc.png",
+      Description:"об установлении тарифов на подключение (тех. присоединение) к системе водоснабзения на 2019 год."
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
--- src/app/services/models.ts
+++ src/app/services/models.ts
@@ -21,6 +21,7 @@ export class Doc{
     Id:number;
     Name:string;
     Image:string;
+    Description?:string;
     Type?:DocTypes;
     Document?:string;
 }
