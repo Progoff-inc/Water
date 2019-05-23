221922d7e3f5d3ade0f9868f5b56c84ae3560897
Иван Номоконов <171035@edu.fa.ru>
2019-05-23 11:13:19 +0300

    #135: Разработать макет страницы «Раскрытие информации»

--- src/app/full-info/full-info.component.ts
+++ src/app/full-info/full-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { WaterService } from '../services/water.service';
+import { LoadService } from '../services/load.service';
+import { Doc, DocTypes } from '../services/models';
 
 @Component({
   selector: 'full-info',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./full-info.component.less']
 })
 export class FullInfoComponent implements OnInit {
-
-  constructor() { }
+  docs:Doc[];
+  types = {};
+  constructor(private ws:WaterService, private ls:LoadService) {
+    this.types[DocTypes.Props] = "Реквизиты";
+    this.types[DocTypes.Constituent] = "Учредительные документы";
+    this.types[DocTypes.Bookkeeping] = "Бухгалтерская отчетность";
+    this.types[DocTypes.Allowing] = "Разрешительная документация";
+    this.types[DocTypes.Evaluation] = "Специальная оценка условий труда";
+   }
 
   ngOnInit() {
+    this.ls.showLoad = true;
+    this.ws.getTypeDocs([DocTypes.Allowing, DocTypes.Bookkeeping, DocTypes.Constituent, DocTypes.Evaluation, DocTypes.Props]).subscribe(docs => {
+      this.docs = docs;
+      console.log(docs);
+      this.ls.showLoad = false;
+    })
+  }
+
+  getDocs(prop){
+    if(this.docs){
+      return this.docs.filter(x => x.Type == prop);
+    }
+  }
+
+  get docTypes() { 
+    return Object.keys(this.types);
   }
 
 }
