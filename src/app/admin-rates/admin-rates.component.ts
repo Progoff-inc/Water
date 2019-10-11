0ab5a7268710d88a952cb0508c52d4f9b8d2ddeb
IANomokonov <nomokonov.vana@yandex.ru>
2019-10-11 21:06:38 +0300

    #154: загрузка тарифов для редактирования

--- src/app/admin-rates/admin-rates.component.ts
+++ src/app/admin-rates/admin-rates.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { WaterService } from '../services/water.service';
+import { AddService } from '../services/add.service';
+import { ClientTypes } from '../services/models';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-admin-rates',
   templateUrl: './admin-rates.component.html',
   styleUrls: ['./admin-rates.component.less']
 })
-export class AdminRatesComponent implements OnInit {
+export class AdminRatesComponent extends AddService implements OnInit {
 
-  constructor() { }
+  constructor( private _ws: WaterService) { 
+    super();
+  }
 
   ngOnInit() {
+    forkJoin([
+      this._ws.getRates(ClientTypes.Client),
+      this._ws.getRates(ClientTypes.Business)
+    ]).subscribe(([clientRates, businessRates]) => {
+      this.items = [...clientRates, ...businessRates];
+    })
+    
   }
 
 }
